199fc6338380a4432ad56caf3faa85bf360fee09
Akindakly <107426060+AKindakly@users.noreply.github.com>
2023-03-21 00:38:34 +0100

    Update db-setup.sql

--- server/sql/db-setup.sql
+++ server/sql/db-setup.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS passwordCodes;
 DROP TABLE IF EXISTS messages;
--- DROP TABLE IF EXISTS posts;
-
 
 
 CREATE TABLE users (
@@ -38,9 +36,8 @@ CREATE TABLE messages (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- CREATE TABLE posts (
---     id SERIAL PRIMARY KEY,
---     user_id INT REFERENCES users(id) NOT NULL,
---     post TEXT NOT NULL CHECK (message != ''),
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+INSERT INTO users (first_name, last_name, email, password, profile_pic, bio) VALUES 
+
+('Fernando', 'Rickard', 'juniper0@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://randomuser.me/api/portraits/men/35.jpg', 'Step-Brother. Brother. Certified Public Accountant. Visualize world peace. 🔵⛈⭐️'),
+('Kaine', 'Pratt', 'juniper1@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc2MTM2MjQ3OF5BMl5BanBnXkFtZTcwNDU4NDIxOQ@@._V1_UX172_CR0,0,172,256_AL_.jpg', 'Father-in-Law. Uncle. Entrepreneur. I feel the need for speed. 🥑🕸🗿'),
+('Dana', 'Miller', 'juniper2@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images.pexels.com/photos/594421/pexels-photo-594421.jpeg?h=350&auto=compress&cs=tinysrgb', 'Niece. Great-Granddaughter. Friend to all cats. I am the master of my destiny. I am the captain of my soul. 👊🧺⛲️');
