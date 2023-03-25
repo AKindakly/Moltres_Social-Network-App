ddd2dfd257d95bfda57033bdfcb6e52e2fc198fc
Akindakly <akindakly@gmail.com>
2023-03-25 18:16:40 +0100

    port changed

--- server/sql/db-setup.sql
+++ server/sql/db-setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS passwordCodes;
+DROP TABLE IF EXISTS friendships;
 DROP TABLE IF EXISTS messages;
 
 
@@ -38,10 +39,7 @@ CREATE TABLE messages (
 
 INSERT INTO users (first_name, last_name, email, password, profile_pic, bio) VALUES 
 
-('Fernando', 'Rickard', 'juniper0@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://randomuser.me/api/portraits/men/35.jpg', 'Step-Brother. Brother. Certified Public Accountant. Visualize world peace. 🔵⛈⭐️'),
-('Kaine', 'Pratt', 'juniper1@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc2MTM2MjQ3OF5BMl5BanBnXkFtZTcwNDU4NDIxOQ@@._V1_UX172_CR0,0,172,256_AL_.jpg', 'Father-in-Law. Uncle. Entrepreneur. I feel the need for speed. 🥑🕸🗿'),
-<<<<<<< HEAD
-('Dana', 'Miller', 'juniper2@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images.pexels.com/photos/594421/pexels-photo-594421.jpeg?h=350&auto=compress&cs=tinysrgb', 'Niece. Great-Granddaughter. Friend to all cats. I am the master of my destiny. I am the captain of my soul. 👊🧺⛲️');
-=======
-('Dana', 'Miller', 'juniper2@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images.pexels.com/photos/594421/pexels-photo-594421.jpeg?h=350&auto=compress&cs=tinysrgb', 'Niece. Great-Granddaughter. Friend to all cats. I am the master of my destiny. I am the captain of my soul. 👊🧺⛲️');
->>>>>>> f8b38a1 (website tested and improved)
+('Fernando', 'Rickard', 'juniper0@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://randomuser.me/api/portraits/men/35.jpg', 'Step-Brother. Brother. Certified Public Accountant. Visualize world peace.'),
+('Kaine', 'Pratt', 'juniper1@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc2MTM2MjQ3OF5BMl5BanBnXkFtZTcwNDU4NDIxOQ@@._V1_UX172_CR0,0,172,256_AL_.jpg', 'Father-in-Law. Uncle. Entrepreneur. I feel the need for speed.'),
+('Dana', 'Miller', 'juniper2@example.com', '$2a$10$rgkdge0a6V2S6Jqwa7cqQuSpzkiWEvuI2UYaqwl50oGGKFz/15gF.', 'https://images.pexels.com/photos/594421/pexels-photo-594421.jpeg?h=350&auto=compress&cs=tinysrgb', 'Niece. Great-Granddaughter. Friend to all cats. I am the master of my destiny. I am the captain of my soul.');
+
